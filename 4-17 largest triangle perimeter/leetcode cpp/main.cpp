7edf8055f76b7cf26a0409e6de42d15a8a4b7d9f
Leo Curdi <ldcurdi@icloud.com>
2023-04-17 20:22:47 -0700

    solved

---
+++ 4-17 largest triangle perimeter/leetcode cpp/main.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+using std::ostream;
+using std::string;
+using std::vector;
+using std::stringstream;
+
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int result = 0;
+
+        // sort the array
+        sort(nums.begin(), nums.end());
+
+        for (int i = nums.size() - 3; i >= 0; i--) { // iterate through each pissible combo of sides
+
+            if (nums[i] + nums[i + 1] > nums[i + 2] && result == 0) { // check if it will make a valid triangle: big side has to be shorter than 2 smaller sides
+
+                result = nums[i] + nums[i + 1] + nums[i + 2]; // calculate and return the sum
+            }
+        }
+
+        return result;
+    }
+};
+
+int main(void) {
+
+
+
+    return 0;
+}
