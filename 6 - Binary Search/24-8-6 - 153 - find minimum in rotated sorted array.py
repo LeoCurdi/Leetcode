090c3d6bea6cdcdb84e4a826f625bd603c2c5504
LeoCurdi <ldcurdi@icloud.com>
2024-09-13 11:38:14 -0700

    fixed solution for 153

--- 6 - Binary Search/24-8-6 - 153 - find minimum in rotated sorted array.py
+++ 6 - Binary Search/24-8-6 - 153 - find minimum in rotated sorted array.py
@@ -8,15 +8,19 @@ class Solution:
             - array[left] is always higher than array[right], until left reaches the min
             - if middle > right, move to the right half
             - if middle < right, move to the left half
+        - NOTE: if middle lands directly on the minimum, we could miss it, so we need to continuously save middle as a possible min
         """
         left, right = 0, len(nums) -1
-        
+        curMin = float("Inf")
+
         while left < right:
             middle = (left + right) // 2
+            curMin = min(curMin, nums[middle]) # check if middle is a new min
+
             if nums[middle] > nums[right]:
                 left = middle + 1
             else:
                 right = middle - 1
         
         # left meets right at the min
-        return nums[left]
+        return min(nums[left], curMin)
